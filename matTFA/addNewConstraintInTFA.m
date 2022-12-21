e1a127be4d72480952e37ac56236b60aeae8095e
pwendering <philipp.wendering@gmail.com>
2022-12-21 09:42:52 +0200

    Add option to fill in pre-allocated matrix
    * if the argument appendFlag is explicitly set to false, the function
      will not append new entries to the relevant fields, but find the first
      all-zero column or row from the right or bottom, respectively
    * does not interfere with behaviour in functions other than convToTFA
      because the default is to append the relevant model fields

--- matTFA/addNewConstraintInTFA.m
+++ matTFA/addNewConstraintInTFA.m
@@ -1,9 +1,24 @@
-function model = addNewConstraintInTFA(model, CName, CType, CLHS, CRHS)
+function model = addNewConstraintInTFA(model, CName, CType, CLHS, CRHS, appendFlag)
 % This function adds one new constraint to the model, and specifies how the
 % associated variables are involved in this constraint
+
+if ~exist('appendFlag', 'var') || isempty(appendFlag)
+    appendFlag = true;
+end
+
 [num_constr,~] = size(model.A);
-model.constraintNames{num_constr+1,1} = CName; % strcat('DFSEU_',model.mets{i});
-model.constraintType{num_constr+1,1}  = CType; % type of constraint: '<', or '>', or '='
-model.rhs(num_constr+1) = CRHS; % value of the right hand side
-model.A(num_constr+1,CLHS.varIDs) = CLHS.varCoeffs; % coefficients of the involved variables
+if appendFlag
+    model.constraintNames{num_constr+1,1}   = CName; % strcat('DFSEU_',model.mets{i});
+    model.constraintType{num_constr+1,1}    = CType; % type of constraint: '<', or '>', or '='
+    model.rhs(num_constr+1)                 = CRHS; % value of the right hand side
+    model.A(num_constr+1,CLHS.varIDs)       = CLHS.varCoeffs; % coefficients of the involved variables
+else
+    % find last non-zero row and add variable at position of first
+    % all-zero row
+    rowIdx = find(all(model.A,2),1,'last') + 1;
+    model.constraintNames{rowIdx}   = CName; % strcat('DFSEU_',model.mets{i});
+    model.constraintType{rowIdx}    = CType; % type of constraint: '<', or '>', or '='
+    model.rhs(rowIdx)               = CRHS; % value of the right hand side
+    model.A(rowIdx,CLHS.varIDs)     = CLHS.varCoeffs; % coefficients of the involved variables
+end
 end
